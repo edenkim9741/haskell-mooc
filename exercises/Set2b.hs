71310afa97fc9d8c1c1a608cb7d61e2fe21ba03e
eden <fusillading@gmail.com>
2024-11-04 16:39:15 +0900

    Set2b Ex8 solved

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -159,4 +159,4 @@ isPrime' n k
 --   biggestPrimeAtMost 10 ==> 7
 
 biggestPrimeAtMost :: Integer -> Integer
-biggestPrimeAtMost = todo
+biggestPrimeAtMost n = if isPrime n then n else biggestPrimeAtMost (n-1)
